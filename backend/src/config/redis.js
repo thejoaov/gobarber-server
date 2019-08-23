4dd27ec6331d8ad1a8294952f79207cc5ab210ee
thejoaov <jvictorsantos852@gmail.com>
2019-08-22 23:57:30 -0300

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,4 +1,6 @@
 export default {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  url: process.env.REDIS_URL || {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
 };
