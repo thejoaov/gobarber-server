6635f9f767db6240157fbb1ff27f22fdf484119d
João Victor dos Santos <jvictorsantos852@gmail.com>
2019-11-01 14:45:29 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
+    'no-console': 'off',
     'no-unused-vars': [
       'error',
       {
