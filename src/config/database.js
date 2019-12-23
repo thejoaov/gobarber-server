a31eec7d9a7eec49f3083d1b74e86980ee23b029
João Victor dos Santos <jvictorsantos852@gmail.com>
2019-12-23 12:28:01 -0300

    Docker compose fixed, new scripts

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('../bootstrap');
 
 module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME || 'postgres',
   storage: './__tests__/database.sqlite',
   logging: false,
   define: {
