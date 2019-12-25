97088cfa7655a3419001a9fcb74f80cbf26a1cc8
João Victor dos Santos <jvictorsantos852@gmail.com>
2019-12-25 07:37:33 -0300

    Appointment test is impossible in this situation

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -2,18 +2,12 @@ import faker from 'faker';
 import { factory } from 'factory-girl';
 
 import User from '../src/app/models/User';
-import Appointment from '../src/app/models/Appointment';
 
 factory.define('User', User, {
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
-  provider: faker.random.boolean(),
-});
-
-factory.define('Appointment', Appointment, {
-  date: faker.date.recent(),
-  provider_id: faker.random.number(),
+  provider: true,
 });
 
 export default factory;
